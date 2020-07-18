0fb459971f28028c076776f6af372275431299bd
Bluemooses <60531407+Bluemooses@users.noreply.github.com>
2020-07-18 12:07:06 -0500

    No text decorate on buttons please

--- src/Components/Body/Body.js
+++ src/Components/Body/Body.js
@@ -5,7 +5,7 @@ export default function Body() {
   return (
     <div class="card">
       <h2 className="welcome">Software Engineer For Hire</h2>
-      <a href="https://docs.google.com/document/d/13BZdbKvktNUin53yJc1FyE0NNxPzWhnQjRLqAUzWWhA">
+      <a className="resumeLink" href="https://docs.google.com/document/d/13BZdbKvktNUin53yJc1FyE0NNxPzWhnQjRLqAUzWWhA">
         <button className="buton">See My Resume</button>
       </a>
     </div>
