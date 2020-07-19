5aa5667fd196d319579f592dc5e26b3512d75bc6
Bluemooses <60531407+Bluemooses@users.noreply.github.com>
2020-07-19 11:21:23 -0500

    Used function to open URLS in new Tabs

--- src/Components/Footer/Footer.js
+++ src/Components/Footer/Footer.js
@@ -8,21 +8,37 @@ import {
 } from "@fortawesome/free-brands-svg-icons";
 
 export default function Footer() {
+  function openInNewTab(url) {
+    var win = window.open(url, "_blank");
+    win.focus();
+  }
   return (
     <div class="social-container">
       <div className="heading">
         <h4>Check Me Out</h4>
       </div>
       <a
-        href="https://www.linkedin.com/in/jacob-brenke-aab554173/"
+        onClick={() => {
+          openInNewTab("https://www.linkedin.com/in/jacob-brenke-aab554173/");
+        }}
         className="linkedin social"
       >
         <FontAwesomeIcon icon={faLinkedin} size="2x" />
       </a>
-      <a href="https://github.com/Bluemooses" className="github social">
+      <a
+        onClick={() => {
+          openInNewTab("https://github.com/Bluemooses");
+        }}
+        className="github social"
+      >
         <FontAwesomeIcon icon={faGithub} size="2x" />
       </a>
-      <a href="https://twitter.com/brenkejacob" className="twitter social">
+      <a
+        onClick={() => {
+          openInNewTab("https://twitter.com/brenkejacob");
+        }}
+        className="twitter social"
+      >
         <FontAwesomeIcon icon={faTwitter} size="2x" />
       </a>
     </div>
